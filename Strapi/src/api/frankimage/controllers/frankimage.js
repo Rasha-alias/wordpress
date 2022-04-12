fdf559aecbeb13199669463d633f5ec0ef2c9c0d
rasha-alias <rashaziyad000@gmail.com>
2022-04-12 18:23:39 +0200

    Strapi files

---
+++ Strapi/src/api/frankimage/controllers/frankimage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  frankimage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::frankimage.frankimage');
---
+++ Strapi/src/api/frankimage/routes/frankimage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * frankimage router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::frankimage.frankimage');
